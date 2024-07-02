b939fc5cf2138c1582a9c61ca010bb5ca88031d9
miangel07 <miguelosoriorojas063@gmail.com>
2024-07-01 23:00:22 -0500

    input atomo

---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Input = ({ type, placeholder, id, name, erros, register }) => {
+  return (
+    <div>
+      <input
+        type={type}
+        name={name}
+        id={id}
+        placeholder={placeholder}
+        {...register(name, {
+          required: {
+            value: true,
+            message: `${name} es obligatorio`,
+          },
+        })}
+        className="text-black w-full h-[32px] rounded-lg  outline outline-3 outline-none  pl-3
+        border-solid border-2 "
+      />
+
+      {erros[name] && <p className="text-amber-700">{erros[name].message}</p>}
+    </div>
+  );
+};
+
+export default Input;
--- src/components/molecules/Auth/LoginComponents.jsx
+++ src/components/molecules/Auth/LoginComponents.jsx
@@ -5,9 +5,10 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { rol } from "../../../utils/rol.js";
 import Mybutton from "../../atoms/Mybutton.jsx";
+import Input from "../../atoms/Input.jsx";
 
 const LoginComponent = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
 
   const [loginUser, { isSuccess }] = useLoginUserMutation();
   const navigation = useNavigate();
@@ -37,17 +38,14 @@ const LoginComponent = () => {
       console.log("User has logged in, successfully");
       navigation("/home");
     }
-    if (getCookie("authToken")) {
-      setIsAuthenticated(true);
-    }
   }, [isSuccess]);
 
   return (
     <>
       <div className="min-h-screen flex flex-col">
         <div className="flex flex-grow">
-          <div className="w-1/2 flex justify-center items-center bg-gray-100">
-            <div className="card w-full max-w-md shadow-lg rounded-md bg-white bg-opacity-90 p-8">
+          <div className="w-1/2 flex justify-center items-center bg-gray-500">
+            <div className="cardF w-full max-w-md shadow-lg rounded-md bg-white bg-opacity-90 p-8">
               <h1 className="text-3xl font-semibold text-gray-800 text-center">
                 COFFCO
               </h1>
@@ -55,7 +53,7 @@ const LoginComponent = () => {
                 coffee control
               </h2>
 
-              {!isAuthenticated && (
+          
                 <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
                   <div className="form-control">
                     <label className="label">
@@ -63,21 +61,13 @@ const LoginComponent = () => {
                         Número de documento
                       </span>
                     </label>
-                    <input
-                      id="id"
-                      type="number"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="12324242"
-                      {...register("id", {
-                        required: {
-                          value: true,
-                          message: "Por favor digitar cédula",
-                        },
-                      })}
+                    <Input
+                      id={"id"}
+                      type={"text"}
+                      register={register}
+                      name={"id"}
+                      erros={errors}
                     />
-                    <span className="text-amber-700">
-                      {errors.id?.message && errors.id.message}
-                    </span>
                   </div>
                   <div className="form-control">
                     <label className="label">
@@ -85,20 +75,15 @@ const LoginComponent = () => {
                         Contraseña
                       </span>
                     </label>
-                    <input
-                      id="password"
-                      type="password"
-                      {...register("password", {
-                        required: {
-                          value: true,
-                          message: "Por favor digitar password",
-                        },
-                      })}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+
+                    <Input
+                      id={"password"}
+                      type={"password"}
+                      register={register}
+                      name={"password"}
+                      erros={errors}
                     />
-                    <span className="text-amber-700">
-                      {errors.password?.message && errors.password.message}
-                    </span>
+
                     <label className="label">
                       <a
                         className="label-text-alt link link-hover"
@@ -122,34 +107,11 @@ const LoginComponent = () => {
                     </Mybutton>
                   </div>
                 </form>
-              )}
+          
             </div>
           </div>
         </div>
       </div>
-
-      {isAuthenticated && (
-        <div className="text-amber-700">
-          <h2>You're signed up for your account</h2>
-          <button
-            onClick={() => logout()}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Logout
-          </button>
-        </div>
-      )}
-      {isAuthenticated && (
-        <div className="text-amber-700">
-          <h2>You're signed up for your account</h2>
-          <button
-            onClick={() => roles()}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Logout
-          </button>
-        </div>
-      )}
     </>
   );
 };
