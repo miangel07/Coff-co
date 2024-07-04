e0ce7aaed7e9b9b798a014be16af97b0fb756927
SS-victoria <stiven-cool@hotmail.com>
2024-07-03 23:20:25 -0500

    home nav

--- src/components/molecules/cards/Cards.jsx
+++ src/components/molecules/cards/Cards.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Mybutton from '../../atoms/Mybutton'
 
-const Cards = ({title,span,icon1,icon2,icon3}) => {
+const Cards = ({ title, span, icon1, span1, icon2,span2, icon3,span3 }) => {
   return (
     <>
       <div className="h-full w-full bg-slate-50 rounded-badge pl-2 pt-4 shadow-dark-lg  hover:shadow-shadow-sena transform hover:scale-y-105 hover:scale-x-105  transition duration-300 ">
         <h1 className="text-3xl font-bold font-sans">{title}</h1>
-        <span className='font-calibri'>{span}</span>
+        <span className="font-calibri">{span}</span>
         <div className="space-y-4 pt-2">
-            {/* <IoDocumentAttachSharp className="ml-4 text-3xl" /> */}
+          <div className="flex gap-4">
             {icon1}
-          {/* <HiDocumentMagnifyingGlass className="ml-4 text-3xl" /> */}
+            {span1}
+          </div>
+          <div className="flex gap-4">
             {icon2}
-          {/* <MdEditDocument className="ml-4 text-3xl" /> */}
-          {icon3}
+            {span2}
+          </div>
+          <div className="flex gap-4">
+            {icon3}
+            {span3}
+          </div>
         </div>
         <div className="pl-4 pt-3 pb-2">
           <Mybutton>Empezar</Mybutton>
@@ -21,7 +27,7 @@ const Cards = ({title,span,icon1,icon2,icon3}) => {
       </div>
     </>
   );
-}
+};
 
 export default Cards
 
--- src/components/plantillas/Home/HomePlantilla.jsx
+++ src/components/plantillas/Home/HomePlantilla.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Navbar from '../../molecules/Navbar/Navbar'
 import Footer from "../../molecules/Footer/Footer";
 import Cards from '../../molecules/cards/Cards'
 import { IoDocumentAttachSharp } from "react-icons/io5";
@@ -8,8 +9,8 @@ import { MdEditDocument } from "react-icons/md";
 const HomePlantilla = () => {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen ">
-      <div className="w-full h-[20px] bg-yellow-200">
-        Aquí la barra de navegación
+      <div className="w-full">
+        <Navbar/>
       </div>
       <div className="w-full max-w-screen-lg flex flex-col md:flex-row bg-blue-300  gap-4 justify-between mt-4">
         <div className="w-full md:w-1/2 mb-4 md:mb-0">
@@ -17,8 +18,11 @@ const HomePlantilla = () => {
             title={"Gestion documental"}
             span={"Organiza y administra tus documentos"}
             icon1={<IoDocumentAttachSharp className="ml-4 text-3xl" />}
+            span1={"Sube y almacena tus documentos"}
             icon2={<HiDocumentMagnifyingGlass className="ml-4 text-3xl" />}
+            span2={"Busca y accede a tus archivos"}
             icon3={<MdEditDocument className="ml-4 text-3xl" />}
+            span3={"Edita tus documentos"}
           />
         </div>
         <div className="w-full md:w-1/2 h-64 bg-green-300">
