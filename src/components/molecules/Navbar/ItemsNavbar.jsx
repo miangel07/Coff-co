213c35206cc1fe88365ec6a787dad561ef6cfb71
miangel07 <miguelosoriorojas063@gmail.com>
2024-08-13 23:47:42 -0500

    iniciando menu

--- src/components/molecules/Navbar/ItemsNavbar.jsx
+++ src/components/molecules/Navbar/ItemsNavbar.jsx
@@ -1,21 +1,7 @@
-/* import React from "react";
+import React from "react";
 
-const ItemsNavbar = ({ icons, items, children ,color }) => {
-  return (
-    <div className="flex flex-row cursor-pointer items-center hover:bg-slate-400 rounded-lg pr-1 gap-1">
-        <span className={`text-2xl text-${color}`} >{icons}</span>
-
-      <div className="flex items-center flex-col justify-between">
-        {items?.map((icon, i) => (
-          <div key={i}>{items[i]}</div>
-        ))}
-      </div>
-      <p className={`line-clamp-1 text-${color} font-calibri font-medium`}>
-      {children}
-
-      </p>
-    </div>
-  );
+const ItemsNavbar = () => {
+  return <div>hola</div>;
 };
 
-export default ItemsNavbar; */
+export default ItemsNavbar;
