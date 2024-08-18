013d443f33f3994843998eabe159276fa5755006
miangel07 <miguelosoriorojas063@gmail.com>
2024-08-18 00:38:38 -0500

    agregando el  CheckboxAtomo

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -5,7 +5,7 @@ const InputAtomo = ({ type, placeholder, id, name, erros, register }) => {
   return (
     <div className="w-full flex flex-col gap-4">
 
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+      <div className="flex w-full h-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 font-sans">
         <Input size={"sm"} type={type} id={id} label={placeholder} name={name}
           {...register(name, {
             required: {
