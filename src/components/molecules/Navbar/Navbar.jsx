e1f863dc3ab53be06d25325a83127ea3b5057c57
miangel07 <miguelosoriorojas063@gmail.com>
2024-09-03 01:13:04 -0500

    ajustes al componete de logos

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -28,6 +28,7 @@ const Navbar = () => {
       className={`top-0 left-0 h-screen text-black transition-all duration-300 ease-in-out ${
         menuAbierto ? "w-64 md:w-48 lg:w-64" : "w-16 md:w-12 lg:w-16"
       }`}
+      
     >
       <div
         className={`p-3 text-black transition-all duration-100 ease-in-out flex ${
