8e3884ddbee34dd80d65afd40fee58a9c3085100
SS-victoria <stiven-cool@hotmail.com>
2024-09-03 01:00:37 -0500

    mejora de home y arreglo de navbar par mejor visualizacion en pantallas lg

--- src/components/plantillas/Home/HomePlantilla.jsx
+++ src/components/plantillas/Home/HomePlantilla.jsx
@@ -3,18 +3,16 @@ import Carousel from "../../organismo/carousel/Carousel";
 import Card from "../../molecules/cards/Cards";
 
 const HomePlantilla = () => {
-
-
   return (
     <>
       {/* contenedor padre */}
-      <div className="flex flex-row gap-3 bg-gray-100 h-full">
+      <div className="flex flex-row gap-3 bg-gray-100 h-full overflow-auto pr-4">
         {/* contenedor 1 del carousel */}
         <div className="flex-1 bg-gray-100 p-4">
           <Carousel />
         </div>
         {/* contenedor 2 */}
-        <div className="flex flex-1 items-center justify-center bg-red-300">
+        <div className="flex flex-1 items-center justify-center bg-gray-100">
           <Card
             imageSrc="/imagenes/gestionDocumental.jpg"
             altText="Woman listening to music"
@@ -24,7 +22,15 @@ const HomePlantilla = () => {
           />
         </div>
         {/* contenedor 3 */}
-        <div className="flex-1 bg-gray-400 p-4"></div>
+        <div className="flex flex-1 items-center justify-center bg-gray-100">
+          <Card
+            imageSrc="/imagenes/escuela_nacional_del_cafe.png"
+            altText="Woman listening to music"
+            footerText="Servicios"
+            buttonText="Ingresar"
+            imageClass="object-contain mx-auto"
+          />
+        </div>
       </div>
     </>
   );
