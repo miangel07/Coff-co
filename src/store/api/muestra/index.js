1de0cc6712f41c2674d7b4dc17d6cb77042b9ef9
daniel22alejandro <danniel10yara@gmail.com>
2024-09-03 16:33:31 -0500

    empezando el crud de muestras en el front

---
+++ src/store/api/muestra/index.js
@@ -0,0 +1,93 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getCookie } from "../../../utils";
+
+// Define el slice de API
+export const muestraApiSlice = createApi({
+    reducerPath: "muestra",
+    baseQuery: fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      headers: {
+        "Content-Type": "application/json",
+        token: `${getCookie("authToken")}`
+      },
+}),
+
+/* creacion de los endpoint del api */
+endpoints: (build) => ({
+
+    //listar muestras
+    getMuestras: build.query({
+      query: () => ({
+        url: "muestra/listar",
+        method: "GET",
+        headers: {
+          token: `${getCookie("authToken")}`,
+        },
+      }),
+      providesTags:['Muestras']
+    }),
+
+    //crear Muestra
+    postMuestra: build.mutation({
+      query:(data)=>({
+        url:'muestra/registrar',
+        method:'POST',
+        body:data,
+        headers:{
+          token:`${getCookie('authToken')}`
+        },
+      }),
+      transformErrorResponse:(response,meta,arg)=>{
+        return{
+          originalArg:arg,
+          error:response.data.message,
+        };
+      },
+      invalidatesTags:['Muestras'],
+    }),
+
+    //actualizar muestra 
+    putMuestra: build.mutation({
+      query:(data)=>({
+        url:`muestra/actualizar/${data.id}`,
+        method:'PUT',
+        body:data,
+        headers:{
+          token:`${getCookie('authToken')}`,
+        },
+      }),
+      transformErrorResponse:(response,meta,arg)=>{
+        return{
+          originalArg: arg,
+          error:response.data.message,
+        }
+      },
+      invalidatesTags:['Muestras']
+    }),
+
+    //eliminar muestra
+    deleteMuestra: build.mutation({
+      query:(id)=>({
+        url:`muestra/eliminar/${id}`,
+        method:'DELETE',
+        headers:{
+          token:`${getCookie('authToken')}`
+        }
+      }),
+      transformErrorResponse:(response,meta,arg)=>{
+        return{
+          originalArg:arg,
+          error: response.data.message,
+        }
+      },
+      invalidatesTags:['Muestras'],
+    })
+  }),
+});
+
+export const { usegetMuestrasQuery, useCrearAmbienteMutation,
+    useActualizarAmbienteMutation, useEliminarAmbienteMutation} = ambientesApiSlice;
+
+
+
+
