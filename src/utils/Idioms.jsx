9590ed143fe80b2005ec9bb91be09f44ec368f19
miangel07 <miguelosoriorojas063@gmail.com>
2024-09-08 20:00:17 -0500

    configuracion del provedor

--- src/utils/Idioms.jsx
+++ src/utils/Idioms.jsx
@@ -1,14 +1,14 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import es from "../language/spanish.json";
-import en from "../language/ english.json";
+import en from "../language/ english.json"; 
 
 const i18n = i18next.use(initReactI18next).init({
-    lng: "es",
+    lng: "es",  
     interpolation: {
-        escapeValue: false,
+        escapeValue: false,  
     },
-    resources: {
+    resources: { 
         es: {
             translation: es,
         },
