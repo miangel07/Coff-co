bf8b2eb0d2b75d1610bc3c6ff899a2048aa3e22a
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-09-12 23:36:21 -0500

    Correccion en salir desde dropdown menu del perfil

--- src/components/molecules/layout/Header.jsx
+++ src/components/molecules/layout/Header.jsx
@@ -20,7 +20,7 @@ const Header = ({ color }) => {
     const handleLogout = () => {
         document.cookie = 'Token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
         console.log('Sesion Finalizada');
-        window.location.reload();
+        navigate("/")
     };
 
     const handleMenuClick = (menu) => {
