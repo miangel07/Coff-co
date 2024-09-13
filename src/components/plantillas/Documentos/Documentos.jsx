07cba58d9b5885106f838128bb6b92550b421336
miangel07 <miguelosoriorojas063@gmail.com>
2024-09-12 20:49:44 -0500

    borrando grafica de documentos

--- src/components/plantillas/Documentos/Documentos.jsx
+++ src/components/plantillas/Documentos/Documentos.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import GraficaDocumento from "../../molecules/graficas/GraficaDocumento";
-import GraficaImages from "../../molecules/graficas/GraficaImages";
+
 import DocumentosOrganismo from "../../organismo/documentos/DocumentosOrganismo";
-import PdfDocuments from "../../organismo/Reportes/PdfDocuments";
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
-import { useGraficaQuery } from "../../../store/api/documentos";
+
 const Documentos = () => {
-  const [imageData, setImageData] = useState([]);
-  const { data, isLoading, isError, error } = useGraficaQuery();
+
 
   return (
     <>
       <div className=" w-full px-10 flex flex-col ">
         <DocumentosOrganismo />
+
       </div>
     </>
   );
