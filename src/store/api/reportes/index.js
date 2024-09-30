f560e6533f7bd5f19c31f299eede6680c66c0a15
miangel07 <miguelosoriorojas063@gmail.com>
2024-09-29 23:09:02 -0500

    iniciando modulo de reportes con grafica

---
+++ src/store/api/reportes/index.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const repotesApi = createApi({
+    reducerPath: "reportes",
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_BASE_URL,
+        headers: {
+            "Content-Type": "application/json",
+            token: `${localStorage.getItem("token")}`,
+        },
+    }),
+    endpoints: (build) => ({
+        getReporte: build.query({
+            query: () => ({
+                url: `/estadisticas/estadistica`,
+                method: "GET",
+            }),
+        }),
+        getReporteRadar: build.query({
+            query: () => ({
+                url: `/estadisticas/radar`,
+                method: "GET",
+            }),
+        }),
+    }),
+})
+
+export const { useGetReporteQuery } = repotesApi;
