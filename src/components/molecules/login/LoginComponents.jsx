3a67b57038f6069f518ef0df23cefdbedddf4d32
daniel22alejandro <danniel10yara@gmail.com>
2024-10-16 11:16:08 -0500

    agregando variable de entorno a login

--- src/components/molecules/login/LoginComponents.jsx
+++ src/components/molecules/login/LoginComponents.jsx
@@ -42,7 +42,7 @@ const LoginComponent = () => {
       // Función para obtener los roles desde el backend
       const fetchRoles = async () => {
         try {
-          const response = await fetch('http://:3000/api/rol/listar'); 
+          const response = await fetch(`${import.meta.env.VITE_BASE_URL}/rol/listar`); 
           const data = await response.json();
           setRoles(data);
         } catch (error) {
